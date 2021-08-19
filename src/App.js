6cb43850525dbd6c638d6bd3f03821b142b38e68
asifhossain2k20 <59142263+asifhossain2k20@users.noreply.github.com>
2021-08-19 16:03:54 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [name,setName]=useState('');
+  const [age,setAge]=useState(0)
+  const [country,setCountry]=useState('');
+  const [position,setPosition]=useState('');
+  const [wage,setWage]=useState(0)
+
+  const displayInfo=()=>{
+    console.log(name,age,country,position,wage)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="information">
+       <label >Name</label>
+       <input type="text" onChange={(event)=>setName(event.target.value)} />
+       <label>Age</label>
+       <input type="number" onChange={(event)=>setAge(event.target.value)} />
+       <label>Country</label>
+       <input type="text" onChange={(event)=>setCountry(event.target.value)} />
+       <label >Position</label>
+       <input type="text" onChange={(event)=>setPosition(event.target.value)} />
+       <label>Wage(Yearly)</label>
+       <input type="number" onChange={(event)=>setWage(event.target.value)} />
+
+       <button onClick={displayInfo}>Add Info</button>
+     </div>
+ 
+ 
     </div>
   );
 }
